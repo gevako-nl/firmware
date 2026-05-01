01759e7c1dce3d68d8299a65cb86d4fa8fba4327
b-smart-home <242258758+b-smart-home@users.noreply.github.com>
2026-05-01 15:11:44 +0200

    Update __init__.py

--- components/thread_provisioning/__init__.py
+++ components/thread_provisioning/__init__.py
@@ -1,14 +1,7 @@
-# Python script to register the custom component and include the header file
 import esphome.codegen as cg
 import esphome.config_validation as cv
 
-# The namespace in your C++ code
-# Replace this if your namespace is different
-thread_prov_ns = cg.esphome_ns.namespace('thread_prov')
-
-# Required schema for component registration
 CONFIG_SCHEMA = cv.Schema({})
 
 async def to_code(config):
-    # This line forces the compiler to include the specific header file
-    cg.add_global(cg.RawExpression('#include "thread_provisioning.h"'))
+    cg.add_global(cg.RawStatement('#include "thread_provisioning.h"'))
